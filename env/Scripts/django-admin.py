bcb160fb7e625e25eacf87cfc4ddd8a6a1236855
Muhammad Waqar <m.waqaryo@gmail.com>
2015-09-02 22:15:00 +0500

    more
    removed everytging

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\waqar shafiq\documents\visual studio 2013\Projects\Practice_DataBaseFun\Practice_DataBaseFun\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
